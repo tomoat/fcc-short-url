4005139b02904ef2db0779e968e9e407f89cbf11
tomato <ljc6qx@163.com>
2017-05-11 14:22:13 +0000

    🌀 remove console log

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/whoami', (req, res) => {
-    console.log(req)
+    // console.log(req)
     const ip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
+     req.connection.socket.remoteAddress
      const info = {
          'ipaddress': ip,
          'language': req.headers["accept-language"].split(',')[0],
          'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
-     };
+     }
     // res.send(info);
     res.json(info)
 })
