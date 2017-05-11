b0305a4c5d9392390fae09e927a634e414407e55
tomato <ljc6qx@163.com>
2017-05-11 14:36:49 +0000

    🍎 add env port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'), err => {
@@ -29,5 +30,4 @@ app.get('/api/whoami', (req, res) => {
     res.json(info)
 })
 
-
-app.listen(8080)
+app.listen(port, console.log('Listening on port: ' + port))
