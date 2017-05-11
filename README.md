f03a370e3b01f4dce526f3c4606d8534cbc31366
tomato <ljc6qx@163.com>
2017-05-11 13:58:24 +0000

    🌱 inital code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Request Header Parser Microservice
+
+用户故事： 用户在浏览器输入该地址后，返回用户的 IP 地址，语言，操作系统等信息。
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/api/whoami', (req, res) => {
+    console.log(req)
+    const ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     const info = {
+         'ipaddress': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+     };
+    // res.send(info);
+    res.json(info)
+})
+
+
+app.listen(8080)
